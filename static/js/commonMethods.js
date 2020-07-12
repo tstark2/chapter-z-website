a6d0c3296bd8f94165eda4e8e3a5f8b28a06e5b6
Todd Stark II <ackilliez@gmail.com>
2020-07-11 21:08:44 -0500

    Update GWRRA logo

--- static/js/commonMethods.js
+++ static/js/commonMethods.js
@@ -32,11 +32,13 @@ export function makeResponsivePicture(name) {
     const pic = document.createElement('picture');
     const source = document.createElement('source');
     const img = document.createElement('img');
-    let pngString = `../img/${name}@2x.png 2x, ../img/${name}@3x.png 3x`;
-    let webpString = `../img/${name}@1x.webp 1x, ../img/${name}@2x.webp 2x, ../img/${name}@3x.webp 3x`;
+    let pngString = `img/${name}@2x.png 2x, img/${name}@3x.png 3x`;
+    let webpString = `img/${name}@1x.webp 1x, img/${name}@2x.webp 2x, img/${name}@3x.webp 3x`;
     
     source.setAttribute('srcset', webpString);
-    img.setAttribute('src', `../img/${name}@1x.png`);
+    source.setAttribute('type', 'image/webp');
+    
+    img.setAttribute('src', `img/${name}@1x.png`);
     img.setAttribute('srcset', pngString);
 
     pic.appendChild(source);
