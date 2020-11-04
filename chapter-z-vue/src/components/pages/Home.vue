dfcb6cd6c5dbc2d70d9697695a966c8c66070d4d
Todd Stark II <ackilliez@gmail.com>
2020-11-04 15:15:06 -0600

    Add mary adams announcement

--- chapter-z-vue/src/components/pages/Home.vue
+++ chapter-z-vue/src/components/pages/Home.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
+        <img src="../../assets/img/mary-adams-annoucement_orig.jpg" />
         <h2 class="purple">Central Illinois Wing Riders</h2>
         <p id="charter">Chartered November 12, 1995</p>
         <p id="gwrraText">Gold Wing Road Riders Association (GWRRA)</p>
         <p class="subheading">Friends for Fun, Safety, and Knowledge</p>
         <p>
-            Monthly socials are the fourth Sunday of the month at the 
+            Monthly socials are the fourth Sunday of the month at the
             <a href="https://www.google.com/maps/place/Germantown+Grille/@40.7632925,-89.4737663,17z/data=!4m5!3m4!1s0x880a5227e6c9c7d5:0x2cf15953358a018b!8m2!3d40.7632885!4d-89.4715776" target="_blank">Germantown Grille, 505 Ten Mile Creek Rd Germantown, Illinois</a>.
             <br />We eat at 5:00 pm with our meeting at 6:00pm.
         </p>
