502bf126910d2e02431acb82d39a7ae2ab6f93c5
Todd Stark II <ackilliez@gmail.com>
2021-07-07 17:15:48 -0500

    remove wing ding notification

--- chapter-z-vue/src/components/pages/Home.vue
+++ chapter-z-vue/src/components/pages/Home.vue
@@ -9,10 +9,6 @@
             <a href="https://www.google.com/maps/place/Germantown+Grille/@40.7632925,-89.4737663,17z/data=!4m5!3m4!1s0x880a5227e6c9c7d5:0x2cf15953358a018b!8m2!3d40.7632885!4d-89.4715776" target="_blank">Germantown Grille, 505 Ten Mile Creek Rd Germantown, Illinois</a>.
             <br />We eat at 5:00 pm with our meeting at 6:00pm.
         </p>
-        <p class="covid">
-          Note:  Due to Wing Ding, the June gathering will be on the 20th not the 27th as would normally be scheduled.
-          See the calendar for details.
-        </p>
         <p><strong>Ice Cream:&nbsp;&nbsp;</strong>Second and Fourth Thursday of the month at 6:00 pm. <br />See calendar for date and location.</p>
 
         <a id="trialMembership" href="https://gwrra-ildistrict.com/trial-membership.html">
